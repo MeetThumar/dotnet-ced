fa1396a347c71c5a3a955dce52b90dadd64682ca
Nikhil <nrakhasiya012@rku.ac.in>
2025-03-11 11:36:24 +0530

    asdasd
    Co-Authored-By: Meet Thumar <72501082+MeetThumar@users.noreply.github.com>
    Co-Authored-By: deep lakdharia <176263398+kanbideep@users.noreply.github.com>

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MNDcars.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
