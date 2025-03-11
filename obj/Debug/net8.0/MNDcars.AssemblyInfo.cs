9b159e2798994bddd7818979c36f44668bafa048
Nikhil <nrakhasiya012@rku.ac.in>
2025-03-11 11:35:48 +0530

    dfdf
    Co-Authored-By: Meet Thumar <72501082+MeetThumar@users.noreply.github.com>
    Co-Authored-By: deep lakdharia <176263398+kanbideep@users.noreply.github.com>

--- obj/Debug/net8.0/MNDcars.AssemblyInfo.cs
+++ obj/Debug/net8.0/MNDcars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MNDcars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e23d79021017619c12ac0e7be4bb03ba81920c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MNDcars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MNDcars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
